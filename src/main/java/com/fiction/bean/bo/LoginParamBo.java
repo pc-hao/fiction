16afb926e78bb57a15fbd86650b3b83bbc9c8cbf
郝鹏程 <20373487@buaa.edu.cn>
2022-11-10 08:57:49 +0800

    upload

--- src/main/java/com/fiction/bean/bo/LoginParamBo.java
+++ src/main/java/com/fiction/bean/bo/LoginParamBo.java
@@ -2,8 +2,10 @@ package com.fiction.bean.bo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class LoginParamBo {
     private String userName;
