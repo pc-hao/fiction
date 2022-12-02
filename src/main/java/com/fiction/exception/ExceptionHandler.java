ce580b72f197685569aecd2c34a030008c88b551
郝鹏程 <20373487@buaa.edu.cn>
2022-12-02 08:48:53 +0800

    尝试修改全局ExceptionHandler函数未果

--- src/main/java/com/fiction/exception/ExceptionHandler.java
+++ src/main/java/com/fiction/exception/ExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.fiction.exception;
 
+import com.fiction.BaseResponse;
+import com.fiction.Enum.BaseCodeEnum;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
-    public String handler() {
-        return "服务异常";
+    public BaseResponse handler(RuntimeException e) {
+        //这段代码没啥用，试了一下发现返回的是404，不知道为什么。
+        return BaseResponse.builder()
+                .code(BaseCodeEnum.FAIL.getCode())
+                .build();
     }
 }
